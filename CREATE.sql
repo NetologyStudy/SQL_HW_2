ffb0ecc096c7a1e912ac254c6ce0d7561830d909
NetologyStudy <ownlove.y@rambler.ru>
2025-01-21 00:51:38 +0300

    Add CREATE.sql, INSERT.sql

---
+++ CREATE.sql
@@ -0,0 +1,126 @@
+-- Создание таблицы "genres"
+CREATE TABLE genres(
+	genre_id SERIAL PRIMARY KEY,
+	genre_name VARCHAR(32) UNIQUE NOT NULL
+);
+
+-- Создание таблицы "artists"
+CREATE TABLE artists(
+	artist_id SERIAL PRIMARY KEY,
+	nickanme VARCHAR(128) NOT NULL
+);
+
+-- Создание таблицы "genres_artists" связь многие-ко-многим
+CREATE TABLE genres_artists(
+	genre_artist_id SERIAL PRIMARY KEY,
+	genre_id INTEGER REFERENCES genres(genre_id),
+	artist_id INTEGER REFERENCES artists(artist_id)
+);
+
+-- Создание таблицы "albums"
+CREATE TABLE albums(
+	album_id SERIAL PRIMARY KEY,
+	album_title VARCHAR(128) NOT NULL,
+	releasy_year DATE NOT NULL
+);
+
+-- Создание таблицы "artists_albums" связь многие-ко-многим
+CREATE TABLE artists_albums(
+	artists_albums_id SERIAL PRIMARY KEY,
+	artist_id INTEGER REFERENCES artists(artist_id),
+	album_id INTEGER REFERENCES albums(album_id)
+);
+
+-- Создание таблицы "tracks"
+CREATE TABLE tracks(
+	track_id SERIAL PRIMARY KEY,
+	track_title VARCHAR(128) NOT NULL,
+	duration INTERVAL NOT NULL,
+	album_id INTEGER REFERENCES albums(album_id)
+);
+
+-- Создание таблицы "collections"
+CREATE TABLE collections(
+	collection_id SERIAL PRIMARY KEY,
+	collection_title VARCHAR(128) NOT NULL
+);
+
+-- Создание таблицы "tracks_collections" связь многие-ко-многим
+CREATE TABLE tracks_collections(
+	track_collection_id SERIAL PRIMARY KEY,
+	track_id INTEGER REFERENCES tracks(track_id),
+	collection_id INTEGER REFERENCES collections(collection_id)
+);
+
+-- Добавление колнны в таблицу "tracks_collections"
+ALTER TABLE
+	collections
+ADD COLUMN
+	release_year DATE;
+
+-- Добвление ограничения в столбец "duration" таблицы "tracks" 
+ALTER TABLE
+	tracks
+ADD CONSTRAINT
+	check_duration_max
+CHECK
+	(duration <= MAKE_INTERVAL(mins => 4));
+
+-- Добвление ограничения в поле "release_year" таблицы "collections" 
+ALTER TABLE
+	collections
+ADD CONSTRAINT
+	check_collection_release_year
+CHECK
+	(EXTRACT(YEAR FROM release_year) >= 2000 AND EXTRACT(YEAR FROM release_year) <= EXTRACT(YEAR FROM CURRENT_DATE));
+
+-- Исправление ошибки в поле "release_year" в таблице "albums"
+ALTER TABLE
+	albums
+RENAME COLUMN
+	releasy_year TO release_year;
+
+-- Добвление ограничения в поле "release_year" таблицы "albums" 
+ALTER TABLE
+	albums
+ADD CONSTRAINT
+	check_album_release_year
+CHECK
+	(EXTRACT(YEAR FROM release_year) >= 2000 AND EXTRACT(YEAR FROM release_year) <= EXTRACT(YEAR FROM CURRENT_DATE));
+
+-- Исправление ошибки в поле "nickname" в таблице "artists"
+ALTER TABLE
+	artists
+RENAME COLUMN
+	nickanme TO nickname;
+
+-- Изменение типа данных в стобце "duration" в таблицу "tracks"
+ALTER TABLE
+	tracks
+ADD COLUMN
+	duration_seconds INTEGER;
+
+UPDATE
+	tracks
+SET
+	duration_seconds = EXTRACT(EPOCH FROM duration);
+
+ALTER TABLE
+	tracks
+DROP COLUMN
+	duration;
+
+ALTER TABLE
+	tracks
+RENAME COLUMN
+	duration_seconds TO duration;
+
+-- Добвление ограничения в поле "duration" таблицы "tracks"
+ALTER TABLE
+	tracks
+ADD CONSTRAINT
+	check_duration_limit
+CHECK
+	(duration <= 240);
+
+
---
+++ INSERT.sql
@@ -0,0 +1,126 @@
+-- Вставка данных в таблицу "genres"
+INSERT INTO
+	genres(genre_name)
+VALUES
+	('Рок'),
+	('Рэп'),
+	('Поп');
+
+-- Вставка данных в таблицу "artists"
+INSERT INTO
+	artists(nickname)
+VALUES
+	('Linkin park'),
+	('Eminem'),
+	('Michael Jackson'),
+	('Elton John');
+
+-- Вставка данных в таблицу "genres_artists"
+INSERT INTO
+	genres_artists(genre_id, artist_id)
+VALUES
+	(1, 1),
+	(2, 2),
+	(3, 3),
+	(3, 4),
+	(1, 4);
+
+-- Вставка данных в таблицу "albums"
+INSERT INTO
+	albums(album_title, release_year)
+VALUES
+	('Meteora', '2003-03-25'),
+	('Encore', '2004-11-12'),
+	('Invincible', '2001-10-30'),
+	('Songs from the West Coast', '2001-10-01'),
+	('The Lockdown Sessions', '2021-10-22');
+
+-- Вставка данных в таблицу "artists_albums"
+INSERT INTO
+	artists_albums(artist_id, album_id)
+VALUES
+	(1, 1),
+	(2, 2),
+	(3, 3),
+	(4, 4),
+	(4, 5);
+
+-- Вставка данных в таблицу "tracks"
+INSERT INTO
+	tracks(track_title, album_id, duration)
+VALUES
+	('My life', 1, 162),
+	('Don''t Stay', 1, 188),
+	('Somewhere I Belong', 1, 214),
+	('Lying from You', 1, 175),
+	('Hit the Floor', 1, 164),
+	('Easier to Run', 1, 204),
+	('Evil Deeds', 2, 237),
+	('Never Enough', 2, 196),
+	('Yellow Brick Road', 2, 173),
+	('Like Toy Soldiers', 2, 179),
+	('Mosh', 2, 165),
+	('Puke', 2, 211),
+	('Unbreakable', 3, 214),
+	('Heartbreaker', 3, 201),
+	('Invincible', 3, 198),
+	('Break of Dawn', 3, 187),
+	('Heaven Can Wait', 3, 229),
+	('2000 Watts', 3, 217),
+	('The Emperor’s New Clothes', 4, 186),
+	('Dark Diamond', 4, 203),
+	('Look Ma, my Hands', 4, 199),
+	('American Triangle', 4, 211),
+	('Original Sin', 4, 239),
+	('Cold Heart', 5, 214),
+	('Always Love You', 5, 171),
+	('Learn to Fly', 5, 190),
+	('After All', 5, 182),
+	('Chosen Family', 5, 212),
+	('The Pink Phantom', 5, 229);
+
+-- Вставка данных в таблицу "collections"
+INSERT INTO
+	collections(collection_title)
+VALUES
+	('Big Ones'),
+	('Number Ones'),
+	('Collision Course'),
+	('Rocket Man: The Definitive Hits');
+
+-- Дополнение данных в таблицу "collections"
+UPDATE collections 
+SET release_year = '2005-07-19'
+WHERE collection_id = 1;
+
+UPDATE collections 
+SET release_year = '2003-11-17'
+WHERE collection_id = 2;
+
+UPDATE collections 
+SET release_year = '2007-03-23'
+WHERE collection_id = 3;
+
+UPDATE collections 
+SET release_year = '2004-11-30'
+WHERE collection_id = 4;
+
+-- Вставка данных в таблицу "tracks_collections"
+INSERT INTO
+	tracks_collections(track_id, collection_id)
+VALUES
+	(1, 1),
+	(3, 1),
+	(6, 2),
+	(7, 3),
+	(8, 3),
+	(12, 2),
+	(14, 2),
+	(16, 2),
+	(18, 1),
+	(19, 1),
+	(21, 3),
+	(23, 2),
+	(25, 3),
+	(27, 3),
+	(29, 1);
